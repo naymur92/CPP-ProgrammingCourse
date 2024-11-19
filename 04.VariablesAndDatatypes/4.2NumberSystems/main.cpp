faba11359f125fc02ce5678b2d3ae46625ec49e9
Naymur Rahman <naymurrahman@yahoo.com>
2024-11-19 15:06:47 +0600

    added linux conf and a chapter of ch4

---
+++ 04.VariablesAndDatatypes/4.2NumberSystems/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1 = 15;          // Decimal
+    int num2 = 017;         // Octal
+    int num3 = 0x0f;        // Hexadecimal
+    int num4 = 0b00001111;  // Binary
+
+    cout << "Number 1 = " << num1 << "\n";
+    cout << "Number 2 = " << num2 << "\n";
+    cout << "Number 3 = " << num3 << "\n";
+    cout << "Number 4 = " << num4 << "\n";
+}
